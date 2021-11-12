09de867399f7372d04042ed31142a947baf73c6d
Oliver Abreu <oabreu@extron.com>
2021-11-12 01:37:21 -0500

    finished MVP

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import {
 	Box,
 	FormHelperText,
 	Input,
-	HStack,
-	Text,
-	Checkbox,
+	VStack,
 	Button,
-	Center
+	Center,
+	Select,
+	Divider,
 } from "@chakra-ui/react";
 import { db } from "./firebase";
 
@@ -18,89 +18,84 @@ export default class App extends React.Component {
 	constructor(props) {
 	super(props);
 	this.state = {
-		name: "",
-		FridayTimes: [
-		// {
-		// 	id: 1,
-		// 	time: "03:00 PM - 04:00 PM"
-		// },
-		// {
-		// 	id: 2,
-		// 	time: "04:00 PM - 05:00 PM"
-		// },
-		// {
-		// 	id: 3,
-		// 	time: "05:00 PM - 06:25 PM"
-		// },
-		{
-			id: 4,
-			time: "08:30 PM - 09:30 PM"
-		}
+		address2: "",
+		shirtSizes: [
+			{
+				id: 1,
+				size: `Small`
+			},
+			{
+				id: 2,
+				size: "Medium"
+			},
+			{
+				id: 3,
+				size: "Large"
+			},
+			{
+				id: 4,
+				size: "Extra Large"
+			},
+			{
+				id: 5,
+				size: "2X Large"
+			},
+			{
+				id: 6,
+				size: "3X Large"
+			},
 		],
-		SaturdayTimes: [
-		// {
-		// 	id: 5,
-		// 	time: `08:00 AM - 08:55 AM`
-		// },
-		// {
-		// 	id: 6,
-		// 	time: "10:20 AM - 11:05 AM"
-		// },
-		{
-			id: 7,
-			time: "12:00 PM - 12:45 PM"
-		},
-		// {
-		// 	id: 8,
-		// 	time: "12:45 PM - 01:30 PM"
-		// },
-		{
-			id: 9,
-			time: "03:25 PM - 04:30 PM"
-		}
-		],
-		selected: []
+		selected: [],
+		orderId: new Date().valueOf(),
+		orderYear: new Date().getFullYear(),
 	};
-	this.handleChange = this.handleChange.bind(this);
-	this.handleSubmit = this.handleSubmit.bind(this);
-	this.cancelSubmit = this.cancelSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.cancelSubmit = this.cancelSubmit.bind(this);
 	}
 	onChange(id) {
-	let selected = this.state.selected;
-	let find = selected.indexOf(id);
+		let selected = this.state.selected.splice(- 1);
+		let find = selected.indexOf(id);
 
-	if (find > -1) {
-		selected.splice(find, 1);
-	} else {
-		selected.push(id);
-	}
-	selected.sort((a, b) => a.id - b.id);
-	this.setState({ selected });
+		if (find > -1) {
+			selected.splice(find, 1);
+		} else {
+			selected.push(id);
+		}
+		selected.sort((a, b) => a.id - b.id);
+		this.setState({ selected });
 	}
 	handleChange(event) {
-	this.setState({
-		name: event.target.value,
-		selected: this.state.selected
-	});
+		this.setState({
+			[event.target.name]: event.target.value	}
+		);
 	}
 	handleSubmit(event) {
 		event.preventDefault();
-		db.collection("volunteers")
+		db.collection("orders")
 			.add({
-				name: this.state.name,
-				availableTimeSlots: this.state.selected
+				orderId: this.state.orderId,
+				orderYear: this.state.orderYear,
+				firstName: this.state.firstName,
+				lastName: this.state.lastName,
+				address1: this.state.address1,
+				address2: this.state.address2,
+				city: this.state.city,
+				state: this.state.state,
+				zip: this.state.zipCode,
+				phoneNumber: this.state.phoneNumber,
+				selectedShirtSize: this.state.selectedShirtSize,
 			})
 			.catch((error) => {
 				alert(error.message);
 			});
+			// alert(
+			// 	`Please allow up 1.5 seconds for this data to be submitted. The page will refresh automatically`
+			// );
 			setTimeout(function () {
 				window.location.reload(false);
-			}, 2000);
-			alert(
-				`Thank you for your willingness to serve during ${
-				this.state.selected.length
-				} time slot(s)!`
-			);
+			}, 1500);
+			
 	}
 
 	cancelSubmit(event) {
@@ -112,13 +107,23 @@ export default class App extends React.Component {
 	window.location.reload(false);
 	}
 	render() {
-	console.log("Name", this.state.name);
-	console.log("Times", this.state.selected);
+	console.log(
+		"First Name:", `${this.state.firstName} \n`,
+		"Last Name:", `${this.state.lastName} \n`,
+		"Address 1", `${this.state.address1} \n`,
+		"Address 2", `${this.state.address2} \n`,
+		"City", `${this.state.city} \n`,
+		"State", `${this.state.state} \n`,
+		"ZIP", `${this.state.zipCode} \n`,
+		"Shirt Size", `${this.state.selectedShirtSize} \n`,
+	)
+	console.log("Shirt Size", this.state.selected[this.state.selected.length-1]);
 		return (
 			<form className="App" onSubmit={this.handleSubmit}>
 				<Flex
 					pt="135px"
-					minHeight="1300px"
+					minHeight="100vh"
+					maxHeight="100%"
 					flexDirection="column"
 					alignItems="center"
 					justifyContent="center"
@@ -126,7 +131,7 @@ export default class App extends React.Component {
 					color="gray.700"
 				>
 					<Center>
-					<FormControl p={4}>
+					<FormControl p={2}>
 					<Box
 						p={5}
 						maxW="sm"
@@ -137,133 +142,411 @@ export default class App extends React.Component {
 						borderColor="gray.200"
 						mb={9}
 					>
-						<Box p={4}>
-						<FormLabel fontSize="2xl">Your Name</FormLabel>
-						<FormHelperText
-							color="gray.500"
-							fontSize="md"
-							textTransform="none"
-							mb={3}
+						<Box 
+							p={4}
+							mb={-5}
 						>
-							Please enter your first and last name
-						</FormHelperText>
+						<FormLabel fontSize="xl">Name</FormLabel>
 						</Box>
-						<Box pl={4} pr={5} pb={4}>
-						<Input
-							value={this.state.value}
-							onChange={this.handleChange}
-							color="gray.500"
-							placeholder="Laura Rabon"
-							textTransform="capitalize"
-							isRequired
-							width="255px"
-							border="1px"
-							borderColor="gray.300"
-							_hover={{
-							border: "1px",
-							borderColor: "gray.300"
-							}}
-						/>
+						<Box pl={4} pr={5} pb={1}>
+							<FormHelperText
+								color="gray.500"
+								fontSize="md"
+								textTransform="none"
+								mb={3}
+							>
+								First Name
+							</FormHelperText>
+							<Input
+								value={this.state.firstName}
+								onChange={this.handleChange}
+								color="#367882"
+								mb={3}
+								name="firstName"
+								fontWeight="bold"
+								placeholder="Laura"
+								textTransform="capitalize"
+								isRequired
+								width="100%"
+								border="1px"
+								borderColor="gray.300"
+								_placeholder={{ 
+									color: 'gray.500', 
+									fontWeight: "normal"
+								}}
+								_active={{
+									borderColor: "#367882"
+								}}
+								_focus={{
+									border: "2px",
+									borderColor: "#367882"
+								}}
+							/>
+							<FormHelperText
+								color="gray.500"
+								fontSize="md"
+								textTransform="none"
+								mb={3}
+							>
+								Last Name
+							</FormHelperText>
+							<Input
+								name="lastName"
+								value={this.state.lastName}
+								onChange={this.handleChange}
+								color="#367882"
+								fontWeight="bold"
+								mb={3}
+								placeholder="Rabon"
+								textTransform="capitalize"
+								isRequired
+								width="100%"
+								border="1px"
+								borderColor="gray.300"
+								_placeholder={{ 
+									color: 'gray.500', 
+									fontWeight: "normal"
+								}}
+								_active={{
+									borderColor: "#367882"
+								}}
+								_focus={{
+									border: "2px",
+									borderColor: "#367882"
+								}}
+							/>
 						</Box>
-					</Box>
-					<Box
-						p={5}
-						maxW="sm"
-						borderWidth="1px"
-						borderRadius="lg"
-						overflow="hidden"
-						bg="white"
-						borderColor="gray.200"
-						mb={5}
-					>
+						<Divider pt={2} pb={2} />
 						<Box p={4}>
-						<FormLabel fontSize="2xl">Your Availability</FormLabel>
+							<FormLabel fontSize="xl">Address</FormLabel>
+							<FormHelperText
+								color="gray.500"
+								fontSize="md"
+								textTransform="none"
+								mb={-1}
+							>
+								Street Address
+							</FormHelperText>
+						</Box>
+						<Box pl={4} pr={5} pb={1}>
+							<Input
+								name="address1"
+								value={this.state.address1}
+								onChange={this.handleChange}
+								color="#367882"
+								mb={3}
+								fontWeight="bold"
+								placeholder="Address 1"
+								textTransform="capitalize"
+								isRequired
+								width="100%"
+								border="1px"
+								borderColor="gray.300"
+								_placeholder={{ 
+									color: 'gray.500', 
+									fontWeight: "normal"
+								}}
+								_active={{
+									borderColor: "#367882"
+								}}
+								_focus={{
+									border: "2px",
+									borderColor: "#367882"
+								}}
+							/>
+							<Input
+								value={this.state.address2}
+								name="address2"
+								onChange={this.handleChange}
+								color="#367882"
+								fontWeight="bold"
+								mb={3}
+								placeholder="Address 2"
+								textTransform="capitalize"
+								width="100%"
+								border="1px"
+								borderColor="gray.300"
+								_placeholder={{ 
+									color: 'gray.500', 
+									fontWeight: "normal"
+								}}
+								_active={{
+									borderColor: "#367882"
+								}}
+								_focus={{
+									border: "2px",
+									borderColor: "#367882"
+								}}
+							/>
+							<FormHelperText
+								color="gray.500"
+								fontSize="md"
+								textTransform="none"
+								mb={3}
+							>
+								City
+							</FormHelperText>
+							<Input
+								value={this.state.city}
+								name="city"
+								onChange={this.handleChange}
+								color="#367882"
+								fontWeight="bold"
+								mb={3}
+								placeholder="Address 2"
+								textTransform="capitalize"
+								isRequired
+								width="100%"
+								border="1px"
+								borderColor="gray.300"
+								_placeholder={{ 
+									color: 'gray.500', 
+									fontWeight: "normal"
+								}}
+								_active={{
+									borderColor: "#367882"
+								}}
+								_focus={{
+									border: "2px",
+									borderColor: "#367882"
+								}}
+							/>
+							<Flex mb={3}>
+								<Flex 
+									flexDirection="column"
+									// border="2px solid red"
+									width="45%"
+									mr={5}
+								>
+									<FormHelperText
+										color="gray.500"
+										fontSize="md"
+										textTransform="none"
+										mb={3}
+									>
+										State
+									</FormHelperText>	
+									<Select
+										value={this.state.state}
+										name="state"
+										onChange={this.handleChange} 
+										placeholder="Select"
+										textTransform="capitalize"
+										isRequired
+										width="100%"
+										border="1px"
+										color="#367882"
+										fontWeight="bold"
+										borderColor="gray.300"
+										_placeholder={{ 
+											color: 'gray.500', 
+											fontWeight: "normal"
+										}}
+										_active={{
+											borderColor: "#367882"
+										}}
+										_focus={{
+											border: "2px",
+											borderColor: "#367882"
+										}}
+									>
+											<option value="AL">Alabama</option>
+												<option value="AK">Alaska</option>
+												<option value="AZ">Arizona</option>
+												<option value="AR">Arkansas</option>
+												<option value="CA">California</option>
+												<option value="CO">Colorado</option>
+												<option value="CT">Connecticut</option>
+												<option value="DE">Delaware</option>
+												<option value="DC">District Of Columbia</option>
+												<option value="FL">Florida</option>
+												<option value="GA">Georgia</option>
+												<option value="HI">Hawaii</option>
+												<option value="ID">Idaho</option>
+												<option value="IL">Illinois</option>
+												<option value="IN">Indiana</option>
+												<option value="IA">Iowa</option>
+												<option value="KS">Kansas</option>
+												<option value="KY">Kentucky</option>
+												<option value="LA">Louisiana</option>
+												<option value="ME">Maine</option>
+												<option value="MD">Maryland</option>
+												<option value="MA">Massachusetts</option>
+												<option value="MI">Michigan</option>
+												<option value="MN">Minnesota</option>
+												<option value="MS">Mississippi</option>
+												<option value="MO">Missouri</option>
+												<option value="MT">Montana</option>
+												<option value="NE">Nebraska</option>
+												<option value="NV">Nevada</option>
+												<option value="NH">New Hampshire</option>
+												<option value="NJ">New Jersey</option>
+												<option value="NM">New Mexico</option>
+												<option value="NY">New York</option>
+												<option value="NC">North Carolina</option>
+												<option value="ND">North Dakota</option>
+												<option value="OH">Ohio</option>
+												<option value="OK">Oklahoma</option>
+												<option value="OR">Oregon</option>
+												<option value="PA">Pennsylvania</option>
+												<option value="RI">Rhode Island</option>
+												<option value="SC">South Carolina</option>
+												<option value="SD">South Dakota</option>
+												<option value="TN">Tennessee</option>
+												<option value="TX">Texas</option>
+												<option value="UT">Utah</option>
+												<option value="VT">Vermont</option>
+												<option value="VA">Virginia</option>
+												<option value="WA">Washington</option>
+												<option value="WV">West Virginia</option>
+												<option value="WI">Wisconsin</option>
+												<option value="WY">Wyoming</option>
+									</Select>
+								</Flex>
+								<Flex flexDirection="column"
+									// border="2px solid red"
+									width="45%"
+								>
+									<FormHelperText
+										color="gray.500"
+										fontSize="md"
+										textTransform="none"
+										mb={3}
+									>
+										ZIP
+									</FormHelperText>
+									<Input
+										name="zipCode"
+										value={this.state.zipCode}
+										onChange={this.handleChange}
+										color="#367882"
+										fontWeight="bold"
+										mb={3}
+										placeholder="27604"
+										textTransform="capitalize"
+										isRequired
+										width="100%"
+										border="1px"
+										borderColor="gray.300"
+										inputprops={{
+											maxLength: 5,
+										}}
+										_placeholder={{ 
+											color: 'gray.500', 
+											fontWeight: "normal"
+										}}
+										_active={{
+											borderColor: "#367882"
+										}}
+										_focus={{
+											border: "2px",
+											borderColor: "#367882"
+										}}
+									/>
+								</Flex>
+							</Flex>
+						</Box>
+						<Divider pt={2} pb={2} />
+						<Box p={4}>
+							<FormLabel fontSize="xl">Phone Number</FormLabel>
+							<FormHelperText
+								color="gray.500"
+								fontSize="md"
+								textTransform="none"
+								mb={-1}
+							>
+								
+							</FormHelperText>
+						</Box>
+						<Box pl={4} pr={5} pb={1}>
+							<Input
+								name="phoneNumber"
+								value={this.state.phoneNumber}
+								onChange={this.handleChange}
+								color="#367882"
+								fontWeight="bold"
+								mb={3}
+								placeholder="919-809-5558"
+								textTransform="capitalize"
+								isRequired
+								width="100%"
+								border="1px"
+								borderColor="gray.300"
+								inputProps={{
+									maxLength: 5,
+								}}
+								_placeholder={{ 
+									color: 'gray.500', 
+									fontWeight: "normal"
+								}}
+								_active={{
+									borderColor: "#367882"
+								}}
+								_focus={{
+									border: "2px",
+									borderColor: "#367882"
+								}}
+							/>
+						</Box>
+						<Divider pt={5} pb={5} />
+						<Box p={4}>
+						<FormLabel fontSize="xl">Shirt</FormLabel>
 						<FormHelperText
 							color="gray.500"
 							fontSize="md"
 							textTransform="none"
 							pr={8}
-							mb={3}
+							// mb={3}
 						>
-							Please select up to three time slots during which you are
-							available to help.
+							Select your size
 						</FormHelperText>
 						</Box>
 						<Box p={4} textAlign="left">
-						<Text mb={15} fontSize="xl" fontWeight="bold">
-							Friday, November 11{" "}
-						</Text>
-						{this.state.FridayTimes.map((item) => {
-							return (
-							<Button
-							key={item.id}
-							textAlign="left"
-							bgColor="#ecc964"
-							mb={2.5}
-							pl={-2}
-							width="245px"
-							_hover={{ transform: "scale(0.98)" }}
-							>
-							<Checkbox
-								size="lg"
-								iconColor="black"
-								colorScheme="white"
-								border="transparent"
-								type="checkbox"
-								value={this.state.selected}
-								onChange={() => this.onChange(item.id)}
-								selected={this.state.selected.includes(item.id)}
-							>
-								<Text width="130%">{item.time}</Text>
-							</Checkbox>
-							</Button>
-							);
-						})}
-						<br />
-						<br />
-						<Text mb={15} fontSize="xl" fontWeight="bold">
-							Saturday, November 12{" "}
-						</Text>
-						{this.state.SaturdayTimes.map((item) => {
-							return (
-							<Button
-							key={item.id}
-							textAlign="left"
-							bgColor="#95b8c0"
-							mb={2.5}
-							pl={0}
-							width="245px"
-							_hover={{ transform: "scale(0.98)" }}
-							>
-							<Checkbox
-								size="lg"
-								iconColor="black"
-								colorScheme="white"
-								border="transparent"
-								value={this.state.selected}
-								_focusVisible={false}
-								type="checkbox"
-								onChange={() => this.onChange(item.id)}
-								selected={this.state.selected.includes(item.id)}
-							>
-								<Text width="130%">{item.time}</Text>
-							</Checkbox>
-							</Button>
-							);
-						})}
+							<Select
+									value={this.state.selectedShirtSize}
+									name="selectedShirtSize"
+									onChange={this.handleChange} 
+									placeholder="Select"
+									textTransform="capitalize"
+									isRequired
+									width="100%"
+									border="1px"
+									color="#367882"
+									fontWeight="bold"
+									borderColor="gray.300"
+									_placeholder={{ 
+										color: 'gray.500', 
+										fontWeight: "normal"
+									}}
+									_active={{
+										borderColor: "#367882"
+									}}
+									_focus={{
+										border: "2px",
+										borderColor: "#367882"
+									}}
+								>
+								{this.state.shirtSizes.map((item) => {
+									return (
+									<option 
+										// value={this.state.selectedShirtSize}
+									>
+										{item.size}
+									</option>
+
+									);
+								})}
+							</Select>
 						</Box>
 					</Box>
-					<FormHelperText color="gray.500" fontSize="sm" pl={8}>
-						Total time slots selected: {this.state.selected.length}
-					</FormHelperText>
 					<Box pt={8}>
-						<HStack pl={5} spacing={5} pb={10}>
+						<VStack pl={0} spacing={5} pb={10}>
 						<Button
-							width="45%"
-							color="red.700"
+							width="100%"
+							color="#871916"
 							bgColor="white"
-							border="1px"
-							borderColor="red.700"
+							border="2px"
+							borderColor="#871916"
 							variant="outline"
 							onClick={this.cancelSubmit}
 							_hover={{
@@ -274,8 +557,8 @@ export default class App extends React.Component {
 						</Button>
 						<Button
 							type="submit"
-							width="45%"
-							bgColor="#38498d"
+							width="100%"
+							bgColor="#142a35"
 							color="white"
 							variant="solid"
 							_hover={{
@@ -284,7 +567,7 @@ export default class App extends React.Component {
 						>
 							Submit Form
 						</Button>
-						</HStack>
+						</VStack>
 					</Box>
 					</FormControl>
 					</Center>
--- src/Header.js
+++ src/Header.js
@@ -4,28 +4,28 @@ import { Flex, Center, Text } from "@chakra-ui/react";
 const Header = () => {
     return (
         <Flex color="gray.800">
-        <Center
-            bg="white"
-            w="100%"
-            h="125px"
-            flexDirection="column"
-            display="inline-flex"
-            alignItems="center"
-            position="fixed"
-            top={0}
-            left={0}
-            p={4}
-            borderBottomColor="gray.100"
-            borderBottomWidth="1px"
-            zIndex="9"
-        >
-            <Text fontSize="3xl" color="black.600">
-            Ladies Retreat 2021
-            </Text>
-            <Text fontSize="3xl" fontWeight="bold" color="black.400">
-            Volunteer Registration
-            </Text>
-        </Center>
+            <Center
+                bg="#142a35"
+                w="100%"
+                h="125px"
+                flexDirection="column"
+                display="inline-flex"
+                alignItems="center"
+                position="fixed"
+                top={0}
+                left={0}
+                p={4}
+                borderBottomColor="gray.100"
+                borderBottomWidth="1px"
+                zIndex="9"
+            >
+                <Text fontSize="2xl" fontWeight="bold" color="#88dcdc">
+                Ladies Retreat 2021
+                </Text>
+                <Text fontSize="3xl" fontWeight="bold" color="white">
+                Shirt Order Form
+                </Text>
+            </Center>
         </Flex>
     );
 };
