7836f02e687ed3ed08e580007ac0cc875508b0a6
Oliver Abreu <oabreu@extron.com>
2021-11-13 18:06:52 -0500

    formated text in results page and changed width

--- src/Results.js
+++ src/Results.js
@@ -46,6 +46,28 @@ export default function Results() {
     let result = propertyValues.filter(function({orderId}) {
         return !this.has(orderId) && this.add(orderId);
     }, new Set)
+
+    function titleCase(str) {
+        str = str.toLowerCase().split(' ');
+        for (var i = 0; i < str.length; i++) {
+            str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+        }
+        return str.join(' ');
+    }
+
+    function formatPhoneNumber(str) {
+        //Filter only numbers from the input
+        let cleaned = ('' + str).replace(/\D/g, '');
+        
+        //Check if the input is of correct length
+        let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    
+        if (match) {
+            return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+        };
+    
+        return null
+    };
     
     useEffect(() => {
         fetchOrders();
@@ -60,16 +82,13 @@ export default function Results() {
             backgroundColor="#f1f1f3"
             color="gray.700"
             textAlign="center"
-            width="2xl"
-            pl={5}
-            pr={5}
+            p={5}
+            maxW="2xl"
         >
             <Center>
                 <Box
-                    width="80vw"
-                    pl={5}
-                    pr={5}
-                    minW="580px"
+                    // width="80vw"
+                    minW="50vw"
                     p={5}
                     pb={7}
                     borderWidth="1px"
@@ -83,10 +102,8 @@ export default function Results() {
                     justifyContent="center"					
                 >
                     <Box
-                        width="80vw"
-                        pl={5}
-                        pr={5}
-                        minW="580px" 
+                        // width="80vw"
+                        minW="50vw"
                         p={4}
                         mb={-5}
                     >
@@ -108,9 +125,9 @@ export default function Results() {
                         return(
                             <Center>
                                 <Box
-                                    width="80vw"
-                                    pl={5}
-                                    pr={5}
+                                    // width="80vw"
+                                    minW="50vw"
+                                    p={6}
                                     borderRadius="lg"
                                     overflow="hidden"
                                     bg="gray.100"
@@ -118,67 +135,66 @@ export default function Results() {
                                     borderColor="gray.500" 
                                     mb={5}
                                     height="100%"
+                                    // border="2px"
+                                    // borderColor="orange"
                                 >
-                                    <Box
-                                        width="80vw"
-                                        pl={5}
-                                        pr={5}
-                                        minW="580px" p="6">
                                         <Box
-                                            width="80vw"
-                                            pl={5}
-                                            pr={5}
-                                            minW="580px" display="flex" alignItems="baseline">
-                                        <Badge 
-                                            borderRadius="full" 
-                                            px="3"
-                                            backgroundColor="#192a34"
-                                            color="#9bdadb"
+                                            width="50%"
+                                            display="flex" 
+                                            alignItems="baseline"
                                         >
-                                            {order.selectedShirtSize}
-                                        </Badge>
+                                            <Badge 
+                                                borderRadius="full" 
+                                                px="3"
+                                                backgroundColor="#192a34"
+                                                color="#9bdadb"
+                                            >
+                                                {order.selectedShirtSize}
+                                            </Badge>
                                         </Box>
 
-                                        <Box
-                                            width="80vw"
-                                            pl={5}
-                                            pr={5}
-                                            minW="580px"
-                                            mt="1"
+                                        <Center
+                                            width="100%"
                                             fontWeight="semibold"
                                             as="h4"
                                             lineHeight="tight"
                                             isTruncated
                                             fontSize="lg"
+                                            // border="2px"
+                                            // borderColor="purple"
                                         >
                                         {order.firstName} {""} {order.lastName}
-                                        </Box>
+                                        </Center>
                                         <VStack p={1}>                                        
                                             <Box
-                                                width="80vw"
-                                                pl={5}
-                                                pr={5}
-                                                minW="580px" as="span" color="gray.600" fontSize="md" mb={-2}>
-                                                {order.address1} {order.address2}
+                                                // width="80vw"
+                                                minW="50vw"
+                                                as="span" 
+                                                color="gray.600" 
+                                                fontSize="md" mb={-2}
+                                            >
+                                                {titleCase(`${order.address1}`.trim())}{titleCase(`${order.address2}`.trim())}
                                             </Box> 
                                             <Box
-                                                width="80vw"
-                                                pl={5}
-                                                pr={5}
-                                                minW="580px" as="span" color="gray.600" fontSize="md">
+                                                // width="80vw"
+                                                minW="50vw"
+                                                as="span" 
+                                                color="gray.600" 
+                                                fontSize="md"
+                                            >
                                                 {order.city}, {order.state} {order.zip} 
                                             </Box>
                                             <Box
-                                                width="80vw"
-                                                pl={5}
-                                                pr={5}
-                                                minW="580px" as="span" color="gray.600" fontSize="md">
-                                                {order.phoneNumber} 
-                                                {/* {formatPhoneNumber} */}
+                                                // width="80vw"
+                                                minW="50vw"
+                                                as="span" 
+                                                color="gray.600" 
+                                                fontSize="md"
+                                            >
+                                                {formatPhoneNumber(`${order.phoneNumber}`)}
                                             </Box> 
                                         </VStack>
                                     </Box>
-                                </Box>
                             </Center>
                         )
                         })
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
 var firebaseApp = firebase.initializeApp({
-    // apiKey: "AIzaSyBP9A-hStf1yzxXoOChxRf6O_uLVJ3Qigc",
-    // authDomain: "lr-volunteerregistration.firebaseapp.com",
-    // projectId: "lr-volunteerregistration",
-    // storageBucket: "lr-volunteerregistration.appspot.com",
-    // messagingSenderId: "766855568975",
-    // appId: "1:766855568975:web:a218e81a08b2fa9cb3d43a",
-    // measurementId: "G-WWJ7CQ96EL"
     apiKey: "AIzaSyDAgytJ70v-L9ve9iFV97wFmxic0NMqFyY",
     authDomain: "t-shirt-registration.firebaseapp.com",
     projectId: "t-shirt-registration",
